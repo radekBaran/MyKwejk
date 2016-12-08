b3ec76878486b86200d5ca1ca344b5952ea4ca4c
Radoslaw Baranan <th3erm4n@gmail.com>
2016-12-08 16:39:46 +0100

    fixed GifController

--- src/main/java/com/radekbaran/controller/GifController.java
+++ src/main/java/com/radekbaran/controller/GifController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,9 +21,9 @@ public class GifController {
 //        return "home";
 //    }
 
-    @GetMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
-        Gif gif = gifRepository.findByName("android-explosion");
+    @GetMapping("/gif/{name}")
+    public String gifDetails(ModelMap modelMap, @PathVariable String name){
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif",gif);
         return "gif-details";
     }
